ccf0a256a99d41e35bf80d3b52565a1f867cbb40
Bishal Gautam <bisalgt@gmail.com>
2021-06-18 19:17:50 +0545

    workin with forms and blog app

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 
-class BlogForm(forms.ModelForm):
+class BlogForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     content = forms.CharField(required=False, widget=forms.Textarea(attrs={"class": "form-control", "rows": 3, "randomAttribue": "Muyna me is bishal"}))
+    
